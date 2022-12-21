a92aef19fdccc9c8db788f0eea0c3b2e008a9b42
Anna <annkrv80@yandex.ru>
2022-12-21 22:10:27 +0700

    Список сотрудников

--- Task002.java
+++ Task002.java
@@ -1,22 +1,31 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Scanner;
 
 public class Task002 {
     public static void main(String[] args) {
-        String worker = "Жданов Иванов Петров Иванов Петров Петров Сидоров Иванов";
-        String[] ar = worker.split(" ");
+        String worker;
+        ArrayList<String> suname = new ArrayList<>();
         HashMap<String, Integer> sortWorker = new HashMap<>();
+        System.out.println("Введите ФИО сотрудника. Чтобы закончить набери end!");
+        do {
+            Scanner in = new Scanner(System.in);
+            worker = in.next();
+            String[] ar = worker.split(" ");
+            suname.add(ar[0]);
+        } while (!worker.equals("end"));
 
-        for (int i = 0; i < ar.length; i++) {
+        for (int i = 0; i < suname.size() - 1; i++) {
             int count = 0;
 
-            for (int j = 0; j < ar.length; j++) {
-                if (ar[i].equals(ar[j])) {
+            for (int j = 0; j < suname.size() - 1; j++) {
+                if (suname.get(i).equals(suname.get(j))) {
                     count++;
                 }
             }
 
-            sortWorker.put(ar[i], count);
+            sortWorker.put(suname.get(i), count);
         }
 
         sortWorker.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
